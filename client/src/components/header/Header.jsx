5aaad1bf3366188d51f003f274dae3d952c96ec8
Ved Jaiswal <vedjaiswal0506@gmail.com>
2023-07-04 19:58:14 +0530

    Header search bar added

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import {AppBar, Toolbar, Box, Typography} from '@mui/material';
 import styled from '@emotion/styled';
+import Search from './Search';
 
 const BoxComponent = styled(Box)`
     margin-left : 12%;
     line-height : 0;
 `
+
+const SubHeading = styled(Typography)` 
+    font-size : 10px;
+    font-style : italic;
+`
+
+const PlusImage = styled('img')({
+    width : 10,
+    height : 10, 
+    marginLeft : 4
+})
 
 function Header() {
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
@@ -14,15 +26,18 @@ function Header() {
   return (
     <div>
         <AppBar style={{background:"#2874f0", height:55}}>
-            <Toolbar>
+            <Toolbar style={{minHeight:55}}>
                 <BoxComponent>
                     <img src={logoURL} alt='logo' style={{ width:75 }}/>
-                    <Box>
-                        <Typography>
-                            Explore Plus
-                        </Typography>
+                    <Box style={{display : "flex"}}>
+                        <SubHeading>
+                            Explore&nbsp;
+                            <Box component='span' style={{color : "#ffe500"}}>Plus</Box>
+                        </SubHeading>
+                        <PlusImage src={subURL} alt='subLogo'></PlusImage>
                     </Box>
                 </BoxComponent>
+                <Search/>
             </Toolbar>
         </AppBar>
     </div>
---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { InputBase, Box } from '@mui/material'
+import styled from '@emotion/styled'
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchContainer = styled(Box)`
+    background : #fff;
+    width : 38%;
+    border-radius : 2px;
+    margin-left : 10px;
+    display : flex;
+`
+
+const InputSearchBase = styled(InputBase)`
+    padding-left : 20px;
+    width : 100%;
+    font-size : unset;
+`
+
+const SearchIconWrapper = styled(Box)`
+    color : blue;
+    padding : 5px;
+`
+
+function Search() {
+  return (
+    <SearchContainer>
+        <InputSearchBase 
+            placeholder='Search for Products, Brands and More'
+        />
+        <SearchIconWrapper>
+            <SearchIcon/>
+        </SearchIconWrapper>
+    </SearchContainer>
+  )
+}
+
+export default Search
