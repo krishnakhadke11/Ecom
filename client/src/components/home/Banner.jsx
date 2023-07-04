501bfc86c9139c79bc044f5b27c384d2978d0767
mounikajindam <mounikajindam7@gmail.com>
2023-07-05 03:22:33 +0530

    carousel

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { styled } from '@mui/material';
+import { bannerData } from '../../constants/data';
+
+const Image = styled('img')({
+    width: '100%',
+    height: 280
+})
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 const Banner = () => {
   return (
-    <div>
-      hello
-    </div>
+    <Carousel responsive={responsive}> 
+    {
+        bannerData.map(data =>(
+            <Image src={data.url} alt="" />
+
+        ))
+}
+</Carousel>
   )
 }
 
