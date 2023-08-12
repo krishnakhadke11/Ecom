650a397ee1d30a26c7ae8ba8aae93d507f838cef
KrishnaKhadke <krishnakhadke11@gmail.com>
2023-08-12 23:35:05 +0530

    payment gateway

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Routes from './routes/route.js';
-
+import { v4 as uuid } from 'uuid';
 const app = express();
 const PORT = 5000;
 
@@ -16,6 +16,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', Routes);
 
+export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+paytmParams['MID'] = process.env.PAYTM_MID,
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+paytmParams['ORDER_ID'] = uuid(),
+paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
+paytmParams['TXN_AMOUNT'] = '100',
+paytmParams['CALLBACK_URL'] = 'http://localhost:5000/callback'
+paytmParams['EMAIL'] = 'mounikajindam7@gmail.com'
+paytmParams['MOBILE_NO'] = '1234567852'
 
 app.listen(PORT, () => {
     console.log(`Server is running successfully on PORT localhost:${PORT}`)
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,7 @@
 import express from  'express';
 import { userSignUp, userLogIn } from '../controller/user-controller.js';
 import { getProducts, getProductById } from '../controller/products-controller.js';
+import {addPaymentGateway,paymentResponse} from '../controller/payment-controller.js'
 const router = express.Router();
 
 router.post('/signup', userSignUp);
@@ -8,5 +9,8 @@ router.post('/login', userLogIn);
 
 router.get('/getProducts',getProducts);
 router.get('/product/:id', getProductById);
+
+router.post('/payment', addPaymentGateway);
+router.post('/callback', paymentResponse);
 
 export default router;
