bb41fc0b12576cbf2f7b89efbfff6aef3402c8af
KrishnaKhadke <krishnakhadke11@gmail.com>
2024-03-12 14:01:49 +0530

    Minor changes

--- server/app.js
+++ server/app.js
@@ -20,11 +20,11 @@ app.use('/', Routes);
 
 export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
 export let paytmParams = {};
-paytmParams['MID'] = process.env.PAYTM_MID,
-paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
-paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
-paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
-paytmParams['ORDER_ID'] = uuid(),
+paytmParams['MID'] = process.env.PAYTM_MID;
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE;
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID;
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID;
+paytmParams['ORDER_ID'] = uuid();
 // paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 // paytmParams['TXN_AMOUNT'] = '100',
 paytmParams['CALLBACK_URL'] = 'http://localhost:5000/callback'
